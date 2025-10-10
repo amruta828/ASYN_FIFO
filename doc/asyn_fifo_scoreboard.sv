b8141704d3f1363151cbf0960a5e428043d32f8a
amruta828 <amrutanmayi6@gmail.com>
2025-10-10 16:47:55 +0530

    Update asyn_fifo_scoreboard.sv

--- doc/asyn_fifo_scoreboard.sv
+++ doc/asyn_fifo_scoreboard.sv
@@ -1,68 +1,87 @@
 class asyn_fifo_scoreboard extends uvm_scoreboard;
 
   `uvm_component_utils(asyn_fifo_scoreboard)
-  
-  asyn_fifo_write_sequence_item write_seq;
-  asyn_fifo_read_sequence_item  read_seq;
-  uvm_tlm_analysis_fifo #( asyn_fifo_write_sequence_item) write_sr;
-  uvm_tlm_analysis_fifo #( asyn_fifo_read_sequence_item )  read_sr;
-  bit [7:0] q[$];  
-  bit [7:0] exp; 
-  int depth = 16; 
 
-  function new(string name = "asyn_fifo_scoreboard", uvm_component parent);    
-    super.new(name, parent);
-  endfunction
  
-  virtual function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    read_sr = new("read_sr", this);
-    write_sr = new("write_sr", this);
+  uvm_tlm_analysis_fifo#(asyn_fifo_read_sequence_item) sco_read_port;
+  uvm_tlm_analysis_fifo#(asyn_fifo_write_sequence_item) sco_write_port;
+
+
+  asyn_fifo_read_sequence_item rseq;
+  asyn_fifo_write_sequence_item wseq;
+
+
+  bit [7:0] w_fifo[$];
+  bit [7:0] r_fifo[$];
+
+  int depth = 16;
+
+  function new(string name="asyn_fifo_scoreboard", uvm_component parent);
+    super.new(name,parent);
+    sco_read_port = new("sco_read_port", this);
+    sco_write_port = new("sco_write_port", this);
   endfunction
-  
+
   virtual task run_phase(uvm_phase phase);
     super.run_phase(phase);
-    forever begin 
-      write_sr.get(write_seq);
-      read_sr.get(read_seq);
-      
-      sco(write_seq, read_seq);
-    end
-  endtask 
-  
-  task sco(asyn_fifo_write_sequence_item write,
-           asyn_fifo_read_sequence_item  read); 
-    if (write.winc) begin
-      if (q.size() == depth) begin
-        if (!write.wfull)
-          `uvm_error("SCOREBOARD", "FIFO full but DUT did NOT assert wfull")
-        else
-          `uvm_info("SCOREBOARD", "WRITE blocked (FIFO full as expected)", UVM_LOW);
-      end
-      q.push_back(write.wdata);
-      `uvm_info("SCOREBOARD", $sformatf("WRITE: Stored %0d (size=%0d)",write.wdata, q.size()), UVM_MEDIUM);
-      end
-    if (read.rinc) begin
-      if (q.size() == 0) begin
-        if (!read.rempty)
-          `uvm_error("SCOREBOARD", "FIFO empty but DUT did NOT assert rempty")
-        else
-          `uvm_info("SCOREBOARD", "READ blocked (FIFO empty as expected)", UVM_LOW);
-      end
 
-      exp = q.pop_front();
-        if (exp != read.rdata)
-          `uvm_error("SCOREBOARD",
-                     $sformatf("DATA MISMATCH: Expected %0d, Got %0d",
-                               exp, read.rdata))
-        else
-          `uvm_info("SCOREBOARD",
-                    $sformatf("match: Got %0d (size=%0d)",
-                              read.rdata, q.size()), UVM_MEDIUM);
+    fork
       
+      forever begin
+  sco_write_port.get(wseq);
+  
+  if (wseq.winc == 1) begin
+    if (w_fifo.size() == depth) begin
+      if (!wseq.wfull) begin
+        `uvm_error("SCOREBOARD", "DUT did not assert wfull when FIFO is full");
+      end 
+      else begin
+        `uvm_info("SCOREBOARD", "WRITE blocked due to FIFO full (as expected)", UVM_LOW);
+      end
+    end 
+    else begin
+      w_fifo.push_back(wseq.wdata);
+      `uvm_info("SCOREBOARD", $sformatf("Collected WDATA = %0d size=%0d", wseq.wdata, w_fifo.size()), UVM_LOW);
+    end
+  end
+
+  compare_if_possible();
+end
+
+     
+      forever begin
+        sco_read_port.get(rseq);
+        if(rseq.rinc==1)
+        if (r_fifo.size() == 0) begin
+          if (!rseq.rempty) begin
+            `uvm_error("SCOREBOARD", "DUT did not assert rempty when FIFO is empty");
+          end
+           else begin
+            `uvm_info("SCOREBOARD", "READ blocked due to FIFO empty (as expected)", UVM_LOW);
+        end 
+      end
+        else begin
+          r_fifo.push_back(rseq.rdata);
+          `uvm_info("SCOREBOARD", $sformatf("Collected RDATA = %0d", rseq.rdata), UVM_LOW);
+        end
+
+        compare_if_possible();
+      end
+    join_none
+  endtask
+
+  task compare_if_possible();
+    while (w_fifo.size() > 0 && r_fifo.size() > 0) begin
+      bit [7:0] wdata = w_fifo.pop_front();
+      bit [7:0] rdata = r_fifo.pop_front();
+
+      if (wdata === rdata) begin
+        `uvm_info("SCOREBOARD", $sformatf("MATCH: wdata=%0d, rdata=%0d ", wdata, rdata), UVM_LOW);
+      end else begin
+        `uvm_error("SCOREBOARD", $sformatf("MISMATCH: wdata=%0d, rdata=%0d", wdata, rdata));
+      end
     end
   endtask
 endclass
 
- 
 
